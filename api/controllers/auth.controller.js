7bebed6aed6c4b4036bfda16426d1643a1c72f07
Matias <mati.delarosa1984@gmail.com>
2024-05-14 18:24:14 -0300

    create signup page ui

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
-import errorHandler from '../utils/error.js';
+import {errorHandler} from '../utils/error.js';
 
 export const signup = async (req, res, next) => {
    const { username, email, password } = req.body;
@@ -9,7 +9,7 @@ export const signup = async (req, res, next) => {
       next(errorHandler(400, 'All fields are required'));
    }
 
-   const hashedPassword = bcryptjs.hashSync(password, 10)
+   const hashedPassword = bcryptjs.hashSync(password, 10);
 
    const newUser = new User({
     username,
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/lib/esm/**/*.js',
   ],
   theme: {
     extend: {},
