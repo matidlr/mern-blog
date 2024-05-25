3eccaa82f2844452b088239e203bb6aa7df8881d
Matias <mati.delarosa1984@gmail.com>
2024-05-25 17:21:24 -0300

    create post complete

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -7,8 +7,6 @@ import {
   uploadBytesResumable,
 } from 'firebase/storage';
 import { app } from '../firebase';
-import { CircularProgressbar } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
 import { 
   updateStart, 
   updateSuccess, 
@@ -51,36 +49,37 @@ export default function DashProfile() {
     }, [imageFile]);
 
     const uploadImage = async () => {
-       setImageFileUploading(true);
-       setImageFileUploadError(null);
-        const storage = getStorage(app);
-        const fileName = new Date().getTime() + imageFile.name;
-        const storageRef = ref(storage, fileName);
-        const uploadTask = uploadBytesResumable(storage, imageFile);
-        uploadTask.on(
-            'state_changed',
-            (snapshot) => {
-              const progress = 
-              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-              setImageFileUploadProgress(progress.toFixed(0));
-            },
-            (error) => {
-              setImageFileUploadError(
-                'Could not upload image (File must be less than 2MB)'
-              );
-              setImageFileUploadProgress(null);
-              setImageFile(null);
-              setImageFileUrl(null);
-              setImageFileUploading(false);
-            },
-            () => {
-              getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=> {
-                setImageFileUrl(downloadURL);
-                setFormData({ ...formData, profilePicture: downloadURL });
-                setImageFileUploading(false);
-              });
-            }
-        );
+      setImageFileUploading(true);
+      setImageFileUploadError(null);
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + imageFile.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, imageFile);
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+  
+          setImageFileUploadProgress(progress.toFixed(0));
+        },
+        (error) => {
+          setImageFileUploadError(
+            'Could not upload image (File must be less than 2MB)'
+          );
+          setImageFileUploadProgress(null);
+          setImageFile(null);
+          setImageFileUrl(null);
+          setImageFileUploading(false);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setImageFileUrl(downloadURL);
+            setFormData({ ...formData, profilePicture: downloadURL });
+            setImageFileUploading(false);
+          });
+        }
+      );
     };
 
     const handleChange = (e) => {
@@ -161,27 +160,7 @@ export default function DashProfile() {
       <form onSubmit={handleSubmit} className="flex flex-col gap-2">
         <input type="file" accept='image/*' onChange={handleImageChange} ref={filePickerRef} hidden/>
         <div className="relative w-32 h-32 self-center cursor-pointer" onClick={()=>filePickerRef.current.click()}>
-        {imageFileUploadProgress && (
-            <CircularProgressbar
-              value={imageFileUploadProgress || 0}
-              text={`${imageFileUploadProgress}%`}
-              strokeWidth={5}
-              styles={{
-                root: {
-                  width: '100%',
-                  height: '100%',
-                  position: 'absolute',
-                  top: 0,
-                  left: 0,
-                },
-                path: {
-                  stroke: `rgba(62, 152, 199, ${
-                    imageFileUploadProgress / 100
-                  })`,
-                },
-              }}
-            />
-          )}
+        
          <img src={imageFileUrl || currentUser.profilePicture} alt="profile"
          className="rounded-full w-full h-full object-cover border-8 border-[lightgray]" />
          </div>
