86b9e7f55bb2547b34ead525c813584a13e19607
Matias <mati.delarosa1984@gmail.com>
2024-05-29 18:24:28 -0300

    add comment section to the post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import CallToAction from '../components/CallToAction';
+import CommentSection from '../components/CommentSection';
 
 export default function PostPage() {
     const { postSlug } = useParams()
@@ -53,6 +54,7 @@ export default function PostPage() {
         <div className="mx-w-4xl mx-auto w-full">
           <CallToAction />
         </div>
+        <CommentSection postId={post._id} />
     </main>
   )
 }
